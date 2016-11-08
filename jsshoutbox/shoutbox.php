fbb550b71b414bb88deed13e6ab46c31f68a2be1
Maha Matin <matin643@gmail.com>
2016-11-07 18:41:44 -0800

    Fixed Typo

--- jsshoutbox/shoutbox.php
+++ jsshoutbox/shoutbox.php
@@ -1,13 +1,1 @@
 
-</<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title> JS shoutbox</title>
-    <link rel="stylesheet" href="CSS/stylesheet.css">
-  </head>
-  <body>
-
-
-  </body>
-</html>
