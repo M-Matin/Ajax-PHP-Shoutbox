77b0eca3a8e72be9d6f5bc0ccb1041c78608879e
Maha Matin <matin643@gmail.com>
2016-11-07 18:48:29 -0800

    JS and jQuery added

--- jsshoutbox/index.php
+++ jsshoutbox/index.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <title> JS shoutbox</title>
     <link rel="stylesheet" href="CSS/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="JS/script.js"></script>
   </head>
   <body>
 
