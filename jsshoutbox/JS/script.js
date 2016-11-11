4f4870ae9c3600d0e7df33ab5df6f993b50e4952
Maha Matin <matin643@gmail.com>
2016-11-11 11:59:03 -0800

    jQuery define variables

--- jsshoutbox/JS/script.js
+++ jsshoutbox/JS/script.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+//eventhandler for what happend when submit btn is clicked
+  $('#submit').on('click', function(){
+//define variable for each form value we are going to use
+    var name = $('#name'),val();
+    var shout = $('#shout').val();
+//getdate() function is what we are going to define later to show the time of user input
+    var date = getdate();
+  });
+});
