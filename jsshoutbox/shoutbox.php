43569fdc8fa68e50aaa941052605878baa33aaf5
Maha Matin <matin643@gmail.com>
2016-11-21 23:03:18 -0800

    passing variables

--- jsshoutbox/shoutbox.php
+++ jsshoutbox/shoutbox.php
@@ -6,5 +6,8 @@ include 'database.php';
 //server side validation
 //using (isset) to see our php function is set
 if (isset($_POST['name']) && isset($_POST['shout'])){//this line says if 'name' and 'shout' are set to php and filled out
-  
+//fuction bellow is going to scape any special charachter that is harmful to database
+  $name = mysqli_real_escape_string($con, $_POST['name']);//variable con is defined in database.php for connecting to database so after conection it will pass name
+  $shout = mysqli_real_escape_string($con, $_POST['shout']);//same thing I did for name variable
+  $date = mysqli_real_escape_string($con, $_POST['date']);//same thing I did for name variable
 }
