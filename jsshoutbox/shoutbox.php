b00f6a64a3d34d8655e0720b3789be3b02f27bf1
Maha Matin <matin643@gmail.com>
2016-11-21 22:54:32 -0800

    server side validation

--- jsshoutbox/shoutbox.php
+++ jsshoutbox/shoutbox.php
@@ -2,3 +2,9 @@
 //connect to database file
 include 'database.php';
  ?>
+ <?php
+//server side validation
+//using (isset) to see our php function is set
+if (isset($_POST['name']) && isset($_POST['shout'])){//this line says if 'name' and 'shout' are set to php and filled out
+  
+}
